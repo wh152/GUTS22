b48814a75a218ff1c27fa338dee3ac5a7ac4e85b
George <radu.george.breahna@gmail.com>
2022-02-12 10:34:08 +0000

    added project template, added to TODO.txt

---
+++ TruthTeller/content.js
@@ -0,0 +1,14 @@
+/*
+1) check page against array of supported sites
+
+2) if supported, get article links based on site scraping rules (html classes, ids, hierarchy)
+including current page link if containing an article, and including any other article links (recommended, top stories etc)
+
+3) send array of article links to server
+
+4) receive ratings array, index for index
+
+5) insert ratings based on insertion rules specific for each site
+*/
+
+alert("YOU GOT MY EXTENSION")
