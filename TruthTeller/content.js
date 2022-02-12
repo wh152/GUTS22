d4652781d13abfd34fa97a53e7ad758b17a3d976
wh152 <2620286H@student.gla.ac.uk>
2022-02-12 13:01:54 +0000

    added default supported browser icon

--- TruthTeller/content.js
+++ TruthTeller/content.js
@@ -1,8 +1,11 @@
+// more sites will be added at the end (get from some list of sites)
 supportedSites = ["theguardian.co.uk", "thesun.co.uk"]
 // this will be the domain name
 var domainName = "example"
-if (!(supportedSites.includes(domainName))){
-    // Have the icon change colour
+if (supportedSites.includes(domainName)){
+    // Not sure what to do here
+} else {
+    chrome.browserAction.setIcon("Buttons/supported.png")
 }
 
 /*
